5cb08c28c3ee4a3b90331b64ba31a27cae14a0f1
andrushkodima@gmail.com <romadumych29299@gmail.com>
2018-03-01 18:23:42 +0200

    1 commit

---
+++ server.js
@@ -0,0 +1,8 @@
+var express=require('express');
+var app=express();
+app.use(express.static(__dirname))
+app.get('',function(req,res){
+	res.sendFile(__dirname+'/index.html')
+})
+app.listen(process.env.PORT||8080)
+console.log('Run server!')
